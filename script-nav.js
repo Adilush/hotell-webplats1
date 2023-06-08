cc8af0f49eb001e4d5816280bbff3cb05019e5fa
Adjan <112627909+Adilush@users.noreply.github.com>
2023-06-08 23:29:51 +0200

    Add files via upload

---
+++ script-nav.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var nav = document.getElementById("myTopnav");
+    if (nav.className === "topnav") {
+      nav.className += " responsive";
+    } else {
+      nav.className = "topnav";
+    }
+  }
